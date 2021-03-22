548199498c019531b2d9370b85ef6aec06964bd3
Joshua <swansonj@mail.usf.edu>
2021-03-21 20:34:27 -0400

    implemented foreign and primary key annotations for distinguishing columns

--- src/main/java/orm/testing/Alien.java
+++ src/main/java/orm/testing/Alien.java
@@ -2,20 +2,25 @@ package orm.testing;
 
 import orm.annotations.Column;
 import orm.annotations.Entity;
+import orm.annotations.Foreign;
+import orm.annotations.Primary;
 
 
 @Entity(name="Alien")
 public class Alien {
 
+    @Primary
     @Column(name = "SSN")
     public int SSN;
 
     @Column(name = "name")
     public String name;
 
+    @Foreign(name = "Mothership")
     @Column(name = "mothership")
     public Mothership mothership;
 
+    @Foreign(name = "RayGun")
     @Column(name = "rayGun")
     public RayGun rayGun;
 
