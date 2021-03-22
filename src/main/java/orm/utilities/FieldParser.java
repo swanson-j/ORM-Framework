400111aac8b991cd012dd958da8b50e0644342ec
Joshua <swansonj@mail.usf.edu>
2021-03-22 01:31:04 -0400

    fixed reflections and annotations bug with finding the primary key of a referenced object

--- src/main/java/orm/utilities/FieldParser.java
+++ src/main/java/orm/utilities/FieldParser.java
@@ -12,7 +12,7 @@ public class FieldParser {
     /*
      *  Returns a statement to insert a row when given a field array
      */
-    public <T> String returnSqlSave(T t, Field[] fields){
+    public <T> String returnSqlSave(T t, Field[] fields) throws IllegalAccessException {
         StringBuilder sb = new StringBuilder();
         sb.append("insert into " + t.getClass().getAnnotation(Entity.class).name() + " values (");
 
@@ -23,8 +23,9 @@ public class FieldParser {
                 // if current field is a foreign key
                 if(fields[i].isAnnotationPresent(Foreign.class)){
 
+                    int finalI = i;
                     // check each field in the foreign key object
-                    Arrays.stream(fields[i].getClass().getDeclaredFields()).forEach(x->{
+                    Arrays.stream(fields[i].get(t).getClass().getDeclaredFields()).forEach(x->{
 
                         // if you find the primary key of the foreign key object
                         if(x.isAnnotationPresent(Primary.class)){
@@ -32,13 +33,13 @@ public class FieldParser {
                             // if the foreign key is a string
                             if(returnDataType(x) == "String"){
                                 try {
-                                    sb.append("\"" + x.get(t) + "\")");
+                                    sb.append("\"" + x.get(fields[finalI].get(t)) + "\")");
                                 } catch (IllegalAccessException e) {
                                     e.printStackTrace();
                                 }
                             } else {
                                 try {
-                                    sb.append(x.get(t) + ")");
+                                    sb.append(x.get(fields[finalI].get(t)) + ")");
                                 } catch (IllegalAccessException e) {
                                     e.printStackTrace();
                                 }
@@ -63,9 +64,15 @@ public class FieldParser {
             } else {
                 // if current field is a foreign key
                 if(fields[i].isAnnotationPresent(Foreign.class)){
-
                     // check each field in the foreign key object
-                    Arrays.stream(fields[i].getClass().getDeclaredFields()).forEach(x->{
+
+                    //TODO: Problem here
+                    System.out.println("Hello");
+
+                    System.out.println(fields[i].get(t));
+
+                    int finalI = i;
+                    Arrays.stream(fields[i].get(t).getClass().getDeclaredFields()).forEach(x->{
 
                         // if you find the primary key of the foreign key object
                         if(x.isAnnotationPresent(Primary.class)){
@@ -73,13 +80,13 @@ public class FieldParser {
                             // if the foreign key is a string
                             if(returnDataType(x) == "String"){
                                 try {
-                                    sb.append("\"" + x.get(t) + "\", ");
+                                    sb.append("\"" + x.get(fields[finalI].get(t)) + "\", ");
                                 } catch (IllegalAccessException e) {
                                     e.printStackTrace();
                                 }
                             } else {
                                 try {
-                                    sb.append(x.get(t) + ", ");
+                                    sb.append(x.get(fields[finalI].get(t)) + ", ");
                                 } catch (IllegalAccessException e) {
                                     e.printStackTrace();
                                 }
